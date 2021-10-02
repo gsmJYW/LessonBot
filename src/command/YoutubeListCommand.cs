65eda3998f5c9e4a67287fe20c0484871523660c
gsmJYW <49026858+gsmJYW@users.noreply.github.com>
2021-10-02 13:02:56 +0900

    feat: 앚튜브목록 명령어 추가

---
+++ src/command/YoutubeListCommand.cs
@@ -0,0 +1,35 @@
+﻿using Discord.Commands;
+using OthelloBot.src.db;
+using OthelloBot.src.embed;
+using System.Data;
+
+namespace OthelloBot.src.command
+{
+    public class YoutubeListCommand : ModuleBase<ShardedCommandContext>
+    {
+        [Command("앚튜브목록")]
+        public async Task YoutubeList(params string[] args)
+        {
+            DataRowCollection rows;
+
+            try
+            {
+                rows = DB.GetYoutubeChannels();
+
+                if (rows.Count == 0)
+                {
+                    await ReplyAsync("구독 중인 앚튜버가 없습니다.");
+                    return;
+                }
+            }
+            catch
+            {
+                await ReplyAsync("목록을 불러오는데 실패 하였습니다.");
+                return;
+            }
+
+            YoutubeListEmbed embed = new(rows);
+            await ReplyAsync(embed: embed.Build());
+        }
+    }
+}
---
+++ src/embed/YoutubeListEmbed.cs
@@ -0,0 +1,18 @@
+﻿using Discord;
+using System.Data;
+
+namespace OthelloBot.src.embed
+{
+    internal class YoutubeListEmbed : EmbedBuilder
+    {
+        public YoutubeListEmbed(DataRowCollection rows)
+        {
+            WithColor(new Color(0xE84F97));
+
+            foreach (DataRow row in rows)
+            {
+                AddField($"{row["name"]}", $"https://www.youtube.com/channel/{row["id"]}");
+            }
+        }
+    }
+}
